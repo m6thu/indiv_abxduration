5007585657b9d36c0cfb1fb313127019f16ff3f4
Mathupanee Oonsivilai <mat.oonsivilai@gmail.com>
2018-10-11 00:09:31 +0700

    working gut model with scenario code embedded for debug

--- model_freqency.R
+++ model_freqency.R
@@ -51,10 +51,10 @@ bact_start <- 500                       # bacteria level (number) for starting
                                         # where big letter = 1*bact_start, singly small = 0.5*bact_start
                                         # small next to big = 0.05*bact_start
 R_thres <- 100                          # R threshold level for tranmissibility
-abxr_killr <- 50                       # amount of r killed by broad spectrum abx r
-abxr_kills <- 50                       # amount of s killed by broad spectrum abx r
-abxs_kills <- 50                       # amount of s killed by narrow spectrum abx s
-r_trans <- 100                           # amount transmitted used as (r_trans*pi_p2)
+abxr_killr <- 500                       # amount of r killed by broad spectrum abx r
+abxr_kills <- 500                       # amount of s killed by broad spectrum abx r
+abxs_kills <- 500                       # amount of s killed by narrow spectrum abx s
+r_trans <- 100                           # amount transmitted
 
 positive_norm_sample <- function(mean, sd){                      
     v<-round(rnorm(1, mean=mean, sd=sd))
@@ -377,7 +377,8 @@ gen_StartBact <- function(los, prob_StartBact){
 }
 
 # 4. Update values for every day (define function)
-nextDay <- function(bed_table, array_LOS, treat_table, colo_table, pi_r1, pi_r2, mu1, mu2, 
+nextDay <- function(bed_table, array_LOS, treat_table, colo_table, 
+                    pi_r1, pi_r2, mu1, mu2, 
                     repop.r1, repop.r2, repop.r3, repop.s1, repop.s2){
     
     S_table <- colo_table[[1]]
@@ -454,7 +455,13 @@ colo_table_filled_long <- nextDay(bed_table= abx.long[[1]], array_LOS=array_LOS_
 ####################6. Visualisation #####################
 
 #present in one space
-par(mfrow=c(4,2))
+#par(mfrow=c(4,2))
+layout(rbind(c(1,1,2,2),
+             c(3,3,4,4),
+             c(5,5,6,6),
+             c(7,7,8,8),
+             c(0,9,9,0)
+             ))
 
 #plots
 #antibiotics pixel
@@ -464,74 +471,62 @@ cols.a<-c('0'='grey', '1'='orange', '2'= 'orange3','3'= 'orange3')
 abx_img.short<-image(1:nrow(abx.mat.short),1:ncol(abx.mat.short),abx.mat.short,col=cols.a, xlab='Time', ylab='Bed No.', main="Short duration of antibiotics")
 abx_img.long<-image(1:nrow(abx.mat.long),1:ncol(abx.mat.long),abx.mat.long,col=cols.a, xlab='Time', ylab='Bed No.', main="Long duration of antibiotics")
 
-#carriage pixel
+#carriage differentiation pixel
 orig_short <- colo_table_filled_short[[2]]
-dim.saved <- dim(orig_short)
-colo_table_filled_short[[2]][colo_table_filled_short[[2]] > 0] <- 1
-colo_table_filled_short[[2]][colo_table_filled_short[[2]] < 0] <- 2
-colo_table_filled_short[[2]] <- as.numeric(colo_table_filled_short[[2]])
-dim(colo_table_filled_short[[2]])<-dim.saved
+colo_table_filled_short <- diff(colo_table_filled_short[[2]])
+dim.saved <- dim(colo_table_filled_short)
+colo_table_filled_short[colo_table_filled_short > 0] <- 1
+colo_table_filled_short[colo_table_filled_short <= 0] <- 2
+colo_table_filled_short <- as.numeric(colo_table_filled_short)
+dim(colo_table_filled_short)<-dim.saved
 
 orig_long <- colo_table_filled_long[[2]]
-colo_table_filled_long[[2]][colo_table_filled_long[[2]] > 0]<-1
-colo_table_filled_long[[2]][colo_table_filled_long[[2]] < 0]<-2
-colo_table_filled_long[[2]]<-as.numeric(colo_table_filled_long[[2]])
-dim(colo_table_filled_long[[2]])<-dim.saved
+colo_table_filled_long <- diff(colo_table_filled_long[[2]])
+colo_table_filled_long[colo_table_filled_long > 0]<-1
+colo_table_filled_long[colo_table_filled_long <= 0]<-2
+colo_table_filled_long<-as.numeric(colo_table_filled_long)
+dim(colo_table_filled_long)<-dim.saved
 
-cols<-c('red','chartreuse3')
-col_img.short<-image(1:nrow(colo_table_filled_short[[2]]),1:ncol(colo_table_filled_short[[2]]), colo_table_filled_short[[2]], col=cols, xlab='Time', ylab='Bed No.')
-col_img.long<-image(1:nrow(colo_table_filled_long[[2]]),1:ncol(colo_table_filled_long[[2]]),colo_table_filled_long[[2]],col=cols, xlab='Time', ylab='Bed No.')
+cols<-c('chartreuse3','red')
+col_img.short<-image(1:nrow(colo_table_filled_short),1:ncol(colo_table_filled_short), colo_table_filled_short, col=cols, xlab='Time', ylab='Bed No.')
+col_img.long<-image(1:nrow(colo_table_filled_long),1:ncol(colo_table_filled_long),colo_table_filled_long,col=cols, xlab='Time', ylab='Bed No.')
+
+#carriage pixel
+# orig_short <- colo_table_filled_short[[2]]
+# colo_table_filled_short <- diff(colo_table_filled_short[[2]])
+# dim.saved <- dim(colo_table_filled_short)
+# colo_table_filled_short[colo_table_filled_short > 0] <- 1
+# colo_table_filled_short[colo_table_filled_short <= 0] <- 2
+# colo_table_filled_short <- as.numeric(colo_table_filled_short)
+# dim(colo_table_filled_short)<-dim.saved
+# 
+# orig_long <- colo_table_filled_long[[2]]
+# colo_table_filled_long <- diff(colo_table_filled_long[[2]])
+# colo_table_filled_long[colo_table_filled_long > 0]<-1
+# colo_table_filled_long[colo_table_filled_long <= 0]<-2
+# colo_table_filled_long<-as.numeric(colo_table_filled_long)
+# dim(colo_table_filled_long)<-dim.saved
+# 
+# cols<-c('chartreuse3','red')
+# col_img.short<-image(1:nrow(colo_table_filled_short),1:ncol(colo_table_filled_short), colo_table_filled_short, col=cols, xlab='Time', ylab='Bed No.')
+# col_img.long<-image(1:nrow(colo_table_filled_long),1:ncol(colo_table_filled_long),colo_table_filled_long,col=cols, xlab='Time', ylab='Bed No.')
+
 
 #increase overtime
 df.short<-as.data.frame(orig_short)
-df.short$totalR<-rep(0, nrow(df.short))
-for (i in 1:nrow(df.short)) {
-    for (j in 1:ncol(df.short)) {
-        if(df.short[i, j]=="sR"|df.short[i, j]=="sr"|df.short[i, j]=="Sr") {
-            df.short$totalR[i] <- df.short$totalR[i] + 1
-        }
-    }
-}
+df.short$totalR<-rowSums(df.short)
 
 df.long<-as.data.frame(orig_long)
-df.long$totalR<-rep(0, nrow(df.long))
-for (i in 1:nrow(df.long)) {
-    for (j in 1:ncol(df.long)) {
-        if (df.long[i,j]=="sR"|df.long[i,j]=="sr"|df.long[i,j]=="Sr") {
-            df.long$totalR[i] <- df.long$totalR[i] + 1
-        }
-    }
-}
+df.long$totalR<-rowSums(df.long)
 
 x <- c(1:n.days)
 y.short <- df.short$totalR/n.bed
 y.long <- df.long$totalR/n.bed
-r.plot.short<-plot(x,y.short, type="l", ylim=c(0,1), xlab="Time", ylab="% patients carrying MDRO")
-lines(x,y.long, col=2)
-r.plot.long<-plot(x, y.long, type="l", ylim=c(0,1), xlab="Time", ylab="% patients carrying MDRO")
+r.plot.short<-plot(x,y.short, type="l", ylim=c(0,bact_slots), xlab="Time", ylab="% patients carrying MDRO")
+#lines(x,y.long, col=2)
+r.plot.long<-plot(x, y.long, type="l", ylim=c(0,bact_slots), xlab="Time", ylab="% patients carrying MDRO")
 
-#total acquisitions 
-df.short$newR <- rep(0, nrow(df.short))
-df.long$newR <- rep(0, nrow(df.long))
-
-for(i in 2:nrow(df.short)){
-    for(j in 1:ncol(df.short)){
-        if((df.short[i, j] == "sR"|df.short[i, j] == "sr"|df.short[i, j] == "Sr") & (df.short[i-1, j] == "S" | df.short[i-1, j] == "s")){
-            df.short$newR[i] <- df.short$newR[i] + 1 
-        }
-    }
-}
-y.short<- df.short$newR
-
-for(i in 2:nrow(df.long)){
-    for(j in 1:ncol(df.long)){
-        if((df.long[i, j] == "sR"|df.long[i, j] == "sr"|df.long[i, j] == "Sr") & (df.long[i-1, j] == "S" | df.long[i-1, j] == "s")){
-            df.long$newR[i] <- df.long$newR[i] + 1 
-        }
-    }
-}
-y.long<- df.long$newR
-
-acq.short<-plot(x,y.short, type='l', xlab="Time", ylab="Number of patients acquiring MDRO")
-lines(x,y.long, col=2)
-acq.long<-plot(x,y.long, type='l', xlab="Time", ylab="Number of patients acquiring MDRO")
+# difference between both plot
+y.diff <- y.long-y.short
+r.plot.long<-plot(x, y.diff, type="l", ylim=c(-bact_slots,bact_slots), xlab="Time", ylab="% patients carrying MDRO")
+abline(h=0)
