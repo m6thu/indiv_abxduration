cd2c2c8261bb4be858cca6a829b808e2c09928d4
Mathupanee Oonsivilai <mat.oonsivilai@gmail.com>
2018-12-20 11:23:14 +0700

    refactored cobweb parameters to be easily to track, added check that params match diff_prevalence. added sections.

--- cobweb_binary.R
+++ cobweb_binary.R
@@ -1,6 +1,6 @@
 #######Modelling Day Project######
 #######Parameter exploration######
-##################################
+################################### Dependencies and functions ################################################
 
 # SAMPLE PARAMETER SPACE 
 # load libraries 
@@ -12,10 +12,20 @@ model <- "binary"
 
 source(paste0("model_", model,".R"))
 
+modelRun.binary <- function (data.df) { #data.df is a dataframe of the parameter values in columns 
+    return(mapply(diff_prevalence, 
+                  data.df[,1], data.df[,2], data.df[,3], data.df[,4], data.df[,5], 
+                  data.df[,6], data.df[,7], data.df[,8], data.df[,9], 
+                  data.df[,10], data.df[,11], data.df[,12], data.df[,13], data.df[,14], data.df[,15], 
+                  data.df[,16], data.df[,17], data.df[,18],data.df[,19],data.df[,20], 
+                  data.df[,21], data.df[,22], data.df[,23], data.df[,24], data.df[,25]
+                  
+    ))
+}
+
+################################## Define parameters and run LHS ####################################
 #list parameters, the probability density functions from which the parameter values will be calculated, and what are the arguments to these functions
-#list parameters together with name, so they are linked 
-#if they don't follow the exact listing of function variables, they seem to feed the wrong range to the wrong variable...
-# MAKE SURE the variable listing and ORDER MATCHES the variable listing input into diff_prevalence
+#list parameters together with name, so they are "linked" or not easily confused
 parameters <- list(
     c("qunif", list(min=5, max=50), "n.bed"), #n.bed; number of beds in the ward
     c("qunif", list(min=3, max=10), "mean.max.los"), #mean.max.los; mean of length of stay (normal distribution)
@@ -49,15 +59,11 @@ q <- unlist(lapply(parameters, function(l) l[[1]]))
 q.arg <- lapply(parameters, function(l) l[2:3])
 factors <- unlist(lapply(parameters, function(l) l[[4]]))
 
-modelRun.binary <- function (data.df) { #data.df is a dataframe of the parameter values in columns 
-    return(mapply(diff_prevalence, 
-                  data.df[,1], data.df[,2], data.df[,3], data.df[,4], data.df[,5], 
-                  data.df[,6], data.df[,7], data.df[,8], data.df[,9], 
-                  data.df[,10], data.df[,11], data.df[,12], data.df[,13], data.df[,14], data.df[,15], 
-                  data.df[,16], data.df[,17], data.df[,18],data.df[,19],data.df[,20], 
-                  data.df[,21], data.df[,22], data.df[,23], data.df[,24], data.df[,25]
-                  
-    ))
+# Test
+# if they don't follow the exact listing of function variables, they seem to feed the wrong range to the wrong variable...
+# MAKE SURE the variable listing and ORDER MATCHES the variable listing input into diff_prevalence
+if(!(sum(factors == parameters_binary) ==  length(parameters_binary))){
+    stop("Test Error: Listing of parameters in cobweb does not match parameters accepted by diff_prevalence function.")
 }
 
 # Use the LHD function to generate a hypercube
@@ -72,6 +78,7 @@ results.binary<-get.results(LHS.binary)
 image_name <- paste0("LHS_", model, "_", format(Sys.time(), "%d%b%Y_%H%M%Z"))
 save.image(paste0(image_name, ".Rdata"))
 
+##################################### Display results ########################################
 #Plot findings: 
 #1. empirical cumulative distribution function used to illustrate the distribution of the model results
 plotecdf(LHS.binary) #outcome has a high probability in the steepest parts of the graph 
--- cobweb_simple.R
+++ cobweb_simple.R
@@ -1,6 +1,7 @@
 #######Modelling Day Project######
 #######Parameter exploration######
 ##################################
+################################### Dependencies and functions ################################################
 
 # SAMPLE PARAMETER SPACE 
 # load libraries 
@@ -13,42 +14,6 @@ model <- "simple"
 
 source(paste0("model_", model,".R"))
 
-#list parameters, the probability density functions from which the parameter values will be calculated, and what are the arguments to these functions
-factors <- parameters_simple
-
-q<- c( 
-    "qunif", #"n.bed", number of beds in the ward
-    "qunif", #"mean.max.los", mean of length of stay 
-    "qunif", #"prob_StartBact_R",probability of initial carriage of resistant organisms
-    "qunif", #"prop_S_nonR", proportion of S in the population of S and ss 
-    "qunif", #"bif", bacterial interference factor 
-    "qunif", # "pi_ssr" probability of being transmitted r to ss (ss—> ssr)
-    "qunif", # "repop.s1" probability of ss repopulated to S 
-    "qunif", # "mu_r", probability of decolonisation 
-    "qunif", # "abx.clear", probability of S becoming ss after being on antibiotics
-    "qunif", # "p", probability of being prescribed antibiotics
-    "qunif", # "short_dur", mean short duration of antibiotics (normal distribution)
-    "qunif", # "long_dur", mean long duration of antibiotics (normal distribution)
-    "qunif"  # "sdDur", standard deviation of duration of antibiotics 
-    )
-
-q.arg <- list(            #set limits of parameters 
-    list(min=5, max=50), #"n.bed", number of beds in the ward
-    list(min=3, max=10), #"mean.max.los", mean of length of stay 
-    list(min=0.01, max=0.99), #"prob_StartBact_R",probability of initial carriage of resistant organisms
-    list(min=0.01, max=0.99), #"prop_S_nonR", proportion of S in the population of S and ss 
-    list(min=0, max=1), #"bif", bacterial interference factor 
-    list(min=0.0001, max=0.05), # "pi_ssr" probability of being transmitted r to ss (ss—> ssr)
-    list(min=0, max=0.03), # "repop.s1" probability of ss repopulated to S (Palleja, Nature Biology, 2018 on gut recovery ~9 months)
-    list(min=0, max=0.03), # "mu_r", probability of decolonisation (Haggai Bar-Yoseph, JAC, 2016, 
-                             #decreasing colonization rates from 76.7% (95% CI = 69.3%–82.8%) at 1 month to 35.2% (95% CI = 28.2%–42.9%) at 12 months of follow-up)
-    list(min=0.01, max=0.9), # "abx.clear", probability of S becoming ss after being on antibiotics
-    list(min=0.1, max=0.9), # "p", probability of being prescribed antibiotics
-    list(min=3, max=7), # "short_dur", mean short duration of antibiotics (normal distribution)
-    list(min=10, max=21), # "long_dur", mean long duration of antibiotics (normal distribution)
-    list(min=1, max=5)  # "sdDur", standard deviation of duration of antibiotics 
-)
-
 modelRun.simple <- function (data.df) { #data.df is a dataframe of the parameter values in columns 
     return(mapply(diff_prevalence, 
                   data.df[,1], data.df[,2], data.df[,3], 
@@ -57,6 +22,37 @@ modelRun.simple <- function (data.df) { #data.df is a dataframe of the parameter
                   data.df[,10], data.df[,11], data.df[,12], 
                   data.df[,13]
     ))
+}
+
+################################## Define parameters and run LHS ####################################
+#list parameters, the probability density functions from which the parameter values will be calculated, and what are the arguments to these functions
+#list parameters together with name, so they are "linked" or not easily confused
+parameters <- list(
+    c("qunif", list(min=5, max=50), "n.bed"),  #"n.bed", number of beds in the ward
+    c("qunif", list(min=3, max=10), "mean.max.los"), #"mean.max.los", mean of length of stay 
+    c("qunif", list(min=0.01, max=0.99), "prob_StartBact_R"),   #"prob_StartBact_R",probability of initial carriage of resistant organisms
+    c("qunif", list(min=0.01, max=0.99), "prop_S_nonR"),        #"prop_S_nonR", proportion of S in the population of S and ss 
+    c("qunif", list(min=0, max=1), "bif"),                #"bif", bacterial interference factor 
+    c("qunif", list(min=0.0001, max=0.05), "pi_ssr"),              # "pi_ssr" probability of being transmitted r to ss (ss—> ssr)
+    c("qunif", list(min=0, max=0.03), "repop.s1"),          # "repop.s1" probability of ss repopulated to S (Palleja, Nature Biology, 2018 on gut recovery ~9 months)
+    c("qunif", list(min=0, max=0.03), "mu_r"),                 # "mu_r", probability of decolonisation (Haggai Bar-Yoseph, JAC, 2016, decreasing colonization rates from 76.7% (95% CI = 69.3%–82.8%) at 1 month to 35.2% (95% CI = 28.2%–42.9%) at 12 months of follow-up)
+    c("qunif", list(min=0.01, max=0.9), "abx.clear"),             # "abx.clear", probability of S becoming ss after being on antibiotics
+    c("qunif", list(min=0.1, max=0.9), "p"),               # "p", probability of being prescribed antibiotics
+    c("qunif", list(min=3, max=7), "short_dur"),           # "short_dur", mean short duration of antibiotics (normal distribution)
+    c("qunif", list(min=10, max=21), "long_dur"),         # "long_dur", mean long duration of antibiotics (normal distribution)
+    c("qunif", list(min=1, max=5), "sdDur")               # "sdDur", standard deviation of duration of antibiotics 
+)    
+
+# arrange parameters in a way LHS will be happy with
+q <- unlist(lapply(parameters, function(l) l[[1]]))
+q.arg <- lapply(parameters, function(l) l[2:3])
+factors <- unlist(lapply(parameters, function(l) l[[4]]))
+
+# Test
+# if they don't follow the exact listing of function variables, they seem to feed the wrong range to the wrong variable...
+# MAKE SURE the variable listing and ORDER MATCHES the variable listing input into diff_prevalence
+if(!(sum(factors == parameters_simple) ==  length(parameters_simple))){
+    stop("Test Error: Listing of parameters in cobweb does not match parameters accepted by diff_prevalence function.")
 }
 
 # Use the LHD function to generate a hypercube 
@@ -71,6 +67,8 @@ results.simple<- get.results(LHS.simple)
 image_name <- paste0("LHS_", model, "_", format(Sys.time(), "%d%b%Y_%H%M%Z"))
 save.image(paste0(image_name, ".Rdata"))
 
+
+##################################### Display results ########################################
 load('LHS_simple_05Dec2018_0211GMT.Rdata')
 
 #Plot findings: 
