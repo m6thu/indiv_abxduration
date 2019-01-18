4ed18a6fe539d9ddef37de940b2c1bb33651f729
Mo Yin <moyin@Mos-MacBook-Pro.local>
2019-01-18 21:23:16 +0700

    updated models

--- show_variability.R
+++ show_variability.R
@@ -12,47 +12,39 @@ source(paste0("model_", model,".R"))
 #######Effect of antibiotic duration on hospitalised patients############
 #########################################################################
 
-mean_short <- 4 #antibiotic short duration
-mean_long <- 14 #antibiotic short duration
-
 if(model == "simple"){
     ################################## Multiple runs comparing between both ####################################
     iterations <- 100
     
+    patient.matrix<-list()
+    los.array<-list()
     abx.short <- list()
     abx.long <- list()
-    array_LOS_short <- list()
-    array_LOS_long <- list()
-    array_StartBact_short <- list()
-    array_StartBact_long <- list()
-    colo_table_filled_short <- list()
-    colo_table_filled_long <- list()
-    
-    short_totalR <- matrix(NA, nrow = n.days, ncol = iterations)
-    long_totalR <- matrix(NA, nrow = n.days, ncol = iterations)
+    colo.matrix<- list()
+    colo_table_filled_short<-list()
+    colo_table_filled_long<-list()
+    short_totalR <- matrix(NA, nrow = n.day, ncol = iterations)
+    long_totalR <- matrix(NA, nrow = n.day, ncol = iterations)
     
     for (i in 1:iterations){
         
         print(i)
+       
+        patient.matrix[[i]]<-patient.table(n.bed = n.bed, n.day=n.day, mean.max.los=mean.max.los, timestep=1)
+        los.array[[i]]<-summary.los(patient.matrix=patient.matrix[[i]])
+        
         #Generate length of stay and antibiotic duration table
-        abx.short[[i]] <- abx.table(n.bed, n.days, mean.max.los, p, meanDur = mean_short)
-        abx.long[[i]] <- abx.table(n.bed, n.days, mean.max.los, p, meanDur = mean_long)
+        abx.short[[i]] <- abx.table(patient.matrix = patient.matrix[[i]], los.array=los.array[[i]], p=p, meanDur=short_dur, sdDur=sdDur, timestep=1)
+        abx.long[[i]] <- abx.table(patient.matrix = patient.matrix[[i]], los.array=los.array[[i]], p=p, meanDur=long_dur, sdDur=sdDur, timestep=1)
         
         #Generate baseline carriage status
-        array_LOS_short[[i]] <- array_LOS_func(los_duration=abx.short[[i]][2])
-        array_LOS_long[[i]] <- array_LOS_func(los_duration=abx.long[[i]][2])
-        
-        #Update values for every day
-        array_StartBact_short[[i]] <- gen_StartBact(los=array_LOS_short[[i]], prob_StartBact_R, prop_S_nonR, n.bed, n.days)
-        array_StartBact_long[[i]] <- gen_StartBact(los=array_LOS_long[[i]], prob_StartBact_R, prop_S_nonR, n.bed, n.days)
+        colo.matrix[[i]] <- colo.table(patient.matrix = patient.matrix[[i]], los.array = los.array[[i]], prob_StartBact_R = prob_StartBact_R, prop_S_nonR = prop_S_nonR)
         
         #output
-        colo_table_filled_short[[i]] <- nextDay(bed_table= abx.short[[i]][[1]], array_LOS=array_LOS_short[[i]], 
-                                                treat_table=abx.short[[i]][[3]], colo_table=array_StartBact_short[[i]], 
-                                                pi_sr=pi_sr, mu_r=mu_r, pi_s=pi_s, pi_r=pi_r, abx.clear=abx.clear)
-        colo_table_filled_long[[i]] <- nextDay(bed_table= abx.long[[i]][[1]], array_LOS=array_LOS_long[[i]], 
-                                               treat_table=abx.long[[i]][[3]], colo_table=array_StartBact_long[[i]], 
-                                               pi_sr=pi_sr, mu_r=mu_r, pi_s=pi_s, pi_r=pi_r, abx.clear=abx.clear)
+        colo_table_filled_short[[i]] <- nextDay(patient.matrix=patient.matrix[[i]], los.array=los.array[[i]], abx.matrix=abx.short[[i]], colo.matrix=colo.matrix[[i]], 
+                                                bif=bif, pi_ssr=pi_ssr, repop.s1=repop.s1, mu_r=mu_r, abx.clear=abx.clear, timestep=1)
+        colo_table_filled_long[[i]] <- nextDay(patient.matrix=patient.matrix[[i]], los.array=los.array[[i]], abx.matrix=abx.long[[i]], colo.matrix=colo.matrix[[i]], 
+                                               bif=bif, pi_ssr=pi_ssr, repop.s1=repop.s1, mu_r=mu_r, abx.clear=abx.clear, timestep=1)
         
         #increase overtime
         df.short <- colo_table_filled_short[[i]]
@@ -66,9 +58,9 @@ if(model == "simple"){
     # Check between 2 simple scenarios
     par(mfrow=c(1,2))
     
-    x <- 1:n.days
+    x <- 1:n.day
     y.short <- short_totalR[,1]/n.bed
-    r.plot.short <- plot(x, y.short, type="l", ylim=c(0,1), main=paste("Short mean abx:", mean_short, 
+    r.plot.short <- plot(x, y.short, type="l", ylim=c(0,1), main=paste("Short mean abx:", short_dur, 
                                                                        "days", "(", round(mean(rowSums(short_totalR)/iterations/n.bed), digits=4), ")"),
                          xlab="Time", ylab="% patients carrying MDRO", col=rgb(0,0,0,alpha=0.1))
     for(i in 2:iterations){
@@ -78,7 +70,7 @@ if(model == "simple"){
     abline(h = mean(rowSums(short_totalR)/iterations/n.bed), col=2, lwd=2)
     
     y.long <- long_totalR[,1]/n.bed
-    r.plot.long <- plot(x, y.long, type="l", ylim=c(0,1), main=paste("Long mean abx:", mean_long, 
+    r.plot.long <- plot(x, y.long, type="l", ylim=c(0,1), main=paste("Long mean abx:", long_dur, 
                                                                      "days", "(", round(mean(rowSums(long_totalR)/iterations/n.bed), digits=4), ")"),
                         xlab="Time", ylab="% patients carrying MDRO", col=rgb(0,0,0,alpha=0.1))
     for(i in 2:iterations){
@@ -91,47 +83,41 @@ if(model == "simple"){
 }else if(model == "binary"){
     ################################## Multiple runs comparing between both ####################################
     iterations <- 100
-    
+
+    patient.matrix<-list()
+    los.array<-list()
     abx.short <- list()
     abx.long <- list()
-    array_LOS_short <- list()
-    array_LOS_long <- list()
-    array_StartBact_short <- list()
-    array_StartBact_long <- list()
-    colo_table_filled_short <- list()
-    colo_table_filled_long <- list()
-    
-    short_totalR <- matrix(NA, nrow = n.days, ncol = iterations)
-    long_totalR <- matrix(NA, nrow = n.days, ncol = iterations)
+    colo.matrix<- list()
+    colo_table_filled_short<-list()
+    colo_table_filled_long<-list()
+    short_totalR <- matrix(NA, nrow = n.day, ncol = iterations)
+    long_totalR <- matrix(NA, nrow = n.day, ncol = iterations)
     
     for (i in 1:iterations){
         
         print(i)
+       
+        patient.matrix[[i]]<-patient.table(n.bed = n.bed, n.day=n.day, mean.max.los=mean.max.los, timestep=1)
+        los.array[[i]]<-summary.los(patient.matrix=patient.matrix[[i]])
+        
         #Generate length of stay and antibiotic duration table
-        abx.short[[i]] <- abx.table(n.bed, n.days, mean.max.los, p.s, p.r.day1, p.r.dayafter, meanDur=mean_short)
-        abx.long[[i]] <- abx.table(n.bed, n.days, mean.max.los, p.s, p.r.day1, p.r.dayafter, meanDur=mean_long)
+        abx.short[[i]] <- abx.table(patient.matrix = patient.matrix[[i]], los.array=los.array[[i]], p.s=p.s, p.r.day1=p.r.day1, p.r.dayafter=p.r.dayafter,
+                                    meanDur.s=short_dur.s, meanDur.r=short_dur.r, sdDur=sdDur, timestep=1)
+        abx.long[[i]] <- abx.table(patient.matrix = patient.matrix[[i]], los.array=los.array[[i]], p.s=p.s, p.r.day1=p.r.day1, p.r.dayafter=p.r.dayafter,
+                                   meanDur.s=long_dur.s, meanDur.r=long_dur.r, sdDur=sdDur, timestep=1)
         
         #Generate baseline carriage status
-        array_LOS_short[[i]] <- array_LOS_func(los_duration=abx.short[[i]][2])
-        array_LOS_long[[i]] <- array_LOS_func(los_duration=abx.long[[i]][2])
+        colo.matrix[[i]] <- colo.table(patient.matrix = patient.matrix[[i]], los.array = los.array[[i]], prob_StartBact_R = prob_StartBact_R, prop_S_nonR = prop_S_nonR,prop_Sr_inR=prop_Sr_inR, prop_sr_inR=prop_sr_inR)
         
-        #Update values for every day
-        array_StartBact_short[[i]] <- gen_StartBact(los=array_LOS_short[[i]], prob_StartBact_R, prop_S_nonR, prop_Sr_inR, prop_sr_inR, n.bed, n.days)
-        array_StartBact_long[[i]] <- gen_StartBact(los=array_LOS_long[[i]], prob_StartBact_R, prop_S_nonR, prop_Sr_inR, prop_sr_inR, n.bed, n.days)
         
         #output
-        colo_table_filled_short[[i]] <- nextDay(bed_table= abx.short[[i]][[1]], array_LOS=array_LOS_short[[i]], 
-                                                treat_table=abx.short[[i]][[3]], colo_table=array_StartBact_short[[i]], 
-                                                pi_r1=pi_r1, pi_r2= pi_r2, mu1=mu1, mu2=mu2, 
-                                                abx.r=abx.r,abx.s=abx.s,
-                                                repop.r1 = repop.r1, repop.r2 = repop.r2, repop.r3 = repop.r3, 
-                                                repop.s1 = repop.s1, repop.s2 = repop.s2,repop.s3 = repop.s3)
-        colo_table_filled_long[[i]] <- nextDay(bed_table= abx.long[[i]][[1]], array_LOS=array_LOS_long[[i]], 
-                                               treat_table=abx.long[[i]][[3]], colo_table=array_StartBact_long[[i]], 
-                                               pi_r1=pi_r1, pi_r2= pi_r2, mu1=mu1, mu2=mu2, 
-                                               abx.r=abx.r,abx.s=abx.s,
-                                               repop.r1 = repop.r1, repop.r2 = repop.r2, repop.r3 = repop.r3, 
-                                               repop.s1 = repop.s1, repop.s2 = repop.s2,repop.s3 = repop.s3)
+        colo_table_filled_short[[i]] <- nextDay(patient.matrix = patient.matrix[[i]], abx.matrix=abx.short[[i]], colo.matrix=colo.matrix[[i]], 
+                                                pi_r1=pi_r1, bif=bif, mu1=mu1, mu2=mu2, repop.r=repop.r, 
+                                                repop.s1=repop.s1, repop.s2=repop.s2,depop.r=depop.r, abx.r=abx.r, abx.s=abx.s, timestep=1)
+        colo_table_filled_long[[i]] <- nextDay(patient.matrix = patient.matrix[[i]], abx.matrix=abx.long[[i]], colo.matrix=colo.matrix[[i]], 
+                                               pi_r1=pi_r1, bif=bif, mu1=mu1, mu2=mu2, repop.r=repop.r, 
+                                               repop.s1=repop.s1, repop.s2=repop.s2,depop.r=depop.r, abx.r=abx.r, abx.s=abx.s, timestep=1)
     
         #increase overtime
         df.short <- colo_table_filled_short[[i]]
@@ -142,17 +128,12 @@ if(model == "simple"){
     
     }
     
-    # Debug edge cases: 43, 71 from run 1431_28Sept2018.RData
-    # Debug cases: 36, 61, 160, 163, 212, 240, 258, 275, 300, 557, 565, 648, 658, 692, 735, 783, 813, 897 from run 1501_28Sept2018.RData
-    # A clean run: 1553_28Sept2018.RData, still uses lists in iteration collection instead of matrix that is easier to summarize from
-    
-    
     # Check between 2 simple scenarios
     par(mfrow=c(1,2))
     
-    x <- 1:n.days
+    x <- 1:n.day
     y.short <- short_totalR[,1]/n.bed
-    r.plot.short <- plot(x, y.short, type="l", ylim=c(0,1), main=paste("Short mean abx:", mean_short, 
+    r.plot.short <- plot(x, y.short, type="l", ylim=c(0,1), main=paste("Short mean abx:", short_dur, 
                          "days", "(", round(mean(rowSums(short_totalR)/iterations/n.bed), digits=4), ")"),
                          xlab="Time", ylab="% patients carrying MDRO", col=rgb(0,0,0,alpha=0.1))
     for(i in 2:iterations){
@@ -162,7 +143,7 @@ if(model == "simple"){
     abline(h = mean(rowSums(short_totalR)/iterations/n.bed), col=2, lwd=2)
     
     y.long <- long_totalR[,1]/n.bed
-    r.plot.long <- plot(x, y.long, type="l", ylim=c(0,1), main=paste("Long mean abx:", mean_long, 
+    r.plot.long <- plot(x, y.long, type="l", ylim=c(0,1), main=paste("Long mean abx:", long_dur, 
                         "days", "(", round(mean(rowSums(long_totalR)/iterations/n.bed), digits=4), ")"),
                         xlab="Time", ylab="% patients carrying MDRO", col=rgb(0,0,0,alpha=0.1))
     for(i in 2:iterations){
@@ -175,52 +156,45 @@ if(model == "simple"){
     ################################## Multiple runs comparing between both ####################################
     iterations <- 100
     
+    patient.matrix<-list()
+    los.array<-list()
     abx.short <- list()
     abx.long <- list()
-    array_LOS_short <- list()
-    array_LOS_long <- list()
-    array_StartBact_short <- list()
-    array_StartBact_long <- list()
-    colo_table_filled_short <- list()
-    colo_table_filled_long <- list()
-    
-    short_totalR <- matrix(NA, nrow = n.days, ncol = iterations)
-    long_totalR <- matrix(NA, nrow = n.days, ncol = iterations)
+    colo.matrix<- list()
+    colo_table_filled_short<-list()
+    colo_table_filled_long<-list()
+    short_totalR <- matrix(NA, nrow = n.day, ncol = iterations)
+    long_totalR <- matrix(NA, nrow = n.day, ncol = iterations)
     
     for (i in 1:iterations){
         
         print(i)
+        
+        patient.matrix[[i]]<-patient.table(n.bed = n.bed, n.day=n.day, mean.max.los=mean.max.los, timestep=1)
+        los.array[[i]]<-summary.los(patient.matrix=patient.matrix[[i]])
+        
         #Generate length of stay and antibiotic duration table
-        abx.short[[i]] <- abx.table(n.bed, n.days, mean.max.los, p.s, p.r.day1, p.r.dayafter, meanDur=mean_short)
-        abx.long[[i]] <- abx.table(n.bed, n.days, mean.max.los, p.s, p.r.day1, p.r.dayafter, meanDur=mean_long)
+        abx.short[[i]] <- abx.table(patient.matrix = patient.matrix[[i]], los.array=los.array[[i]], p.s=p.s, p.r.day1=p.r.day1, p.r.dayafter=p.r.dayafter,
+                                    meanDur.s=short_dur.s, meanDur.r=short_dur.r, sdDur=sdDur, timestep=1)
+        abx.long[[i]] <- abx.table(patient.matrix = patient.matrix[[i]], los.array=los.array[[i]], p.s=p.s, p.r.day1=p.r.day1, p.r.dayafter=p.r.dayafter,
+                                   meanDur.s=long_dur.s, meanDur.r=long_dur.r, sdDur=sdDur, timestep=1)
         
         #Generate baseline carriage status
-        array_LOS_short[[i]] <- array_LOS_func(los_duration=abx.short[[i]][2])
-        array_LOS_long[[i]] <- array_LOS_func(los_duration=abx.long[[i]][2])
-        
-        #Update values for every day
-        array_StartBact_short[[i]] <- gen_StartBact(los=array_LOS_short[[i]], K, t_mean = 4.0826, t_sd = 1.1218, r_mean =1.7031, r_sd = 1.8921, n.beds, n.days)
-        array_StartBact_long[[i]] <- gen_StartBact(los=array_LOS_long[[i]], K, t_mean = 4.0826, t_sd = 1.1218, r_mean =1.7031, r_sd = 1.8921, n.beds, n.days)
+        colo.matrix[[i]] <- colo.table(patient.matrix = patient.matrix[[i]], los.array = los.array[[i]], t_mean=t_mean, t_sd=t_sd, r_mean=r_mean, r_sd=r_sd)
         
         #output
-        colo_table_filled_short[[i]] <- nextDay(bed_table= abx.short[[i]][[1]], array_LOS=array_LOS_short[[i]], 
-                                                treat_table=abx.short[[i]][[3]], colo_table=array_StartBact_short[[i]], 
-                                                pi_r1=pi_r1, pi_r2= pi_r2, mu1=mu1, mu2=mu2, 
-                                                abx.r=abx.r,abx.s=abx.s,
-                                                repop.r1 = repop.r1, repop.r2 = repop.r2, repop.r3 = repop.r3, 
-                                                repop.s1 = repop.s1, repop.s2 = repop.s2)
-        colo_table_filled_long[[i]] <- nextDay(bed_table= abx.long[[i]][[1]], array_LOS=array_LOS_long[[i]], 
-                                               treat_table=abx.long[[i]][[3]], colo_table=array_StartBact_long[[i]], 
-                                               pi_r1=pi_r1, pi_r2= pi_r2, mu1=mu1, mu2=mu2, 
-                                               abx.r=abx.r,abx.s=abx.s,
-                                               repop.r1 = repop.r1, repop.r2 = repop.r2, repop.r3 = repop.r3, 
-                                               repop.s1 = repop.s1, repop.s2 = repop.s2)
+        colo_table_filled_short[[i]] <- nextDay(patient.matrix = patient.matrix[[i]], los.array = los.array[[i]], abx.matrix=abx.short[[i]], colo.matrix=colo.matrix[[i]], 
+                                                pi_r=pi_r, K=K, r_thres=r_thres, r_growth=r_growth, r_trans=r_trans, 
+                                                abxr_killr=abxr_killr, abxr_kills=abxr_kills, abxs_kills=abxs_kills, timestep=1)
+        colo_table_filled_long[[i]] <- nextDay(patient.matrix = patient.matrix[[i]], los.array = los.array[[i]], abx.matrix=abx.long[[i]], colo.matrix=colo.matrix[[i]], 
+                                               pi_r=pi_r, K=K, r_thres=r_thres, r_growth=r_growth, r_trans=r_trans, 
+                                               abxr_killr=abxr_killr, abxr_kills=abxr_kills, abxs_kills=abxs_kills, timestep=1)
         
         #increase overtime
-        df.short <- data.frame(colo_table_filled_short[[i]][2])
+        df.short <- data.frame(colo_table_filled_short[[i]])
         short_totalR[,i]<- rowSums(df.short)
         
-        df.long <- data.frame(colo_table_filled_long[[i]][2])
+        df.long <- data.frame(colo_table_filled_long[[i]])
         long_totalR[,i] <- rowSums(df.long)
         
     }
@@ -228,9 +202,10 @@ if(model == "simple"){
     # Check between 2 simple scenarios
     par(mfrow=c(1,2))
     
-    x <- 1:n.days
+    x <- 1:n.day
     y.short <- short_totalR[,1]/n.bed
-    r.plot.short <- plot(x, y.short, type="l", ylim=c(0,bact_slots), main=paste("Short mean abx:", mean_short, 
+    y.long <- long_totalR[,1]/n.bed
+    r.plot.short <- plot(x, y.short, type="l", ylim=c(0,max(max(y.short),max(y.long))), main=paste("Short mean abx:", short_dur, 
                                                                        "days", "(", round(mean(rowSums(short_totalR)/iterations/n.bed), digits=4), ")"),
                          xlab="Time", ylab="% patients carrying MDRO", col=rgb(0,0,0,alpha=0.1))
     for(i in 2:iterations){
@@ -239,8 +214,7 @@ if(model == "simple"){
     lines(x, rowSums(short_totalR)/iterations/n.bed, col=2)
     abline(h = mean(rowSums(short_totalR)/iterations/n.bed), col=2, lwd=2)
     
-    y.long <- long_totalR[,1]/n.bed
-    r.plot.long <- plot(x, y.long, type="l", ylim=c(0,bact_slots), main=paste("Long mean abx:", mean_long, 
+    r.plot.long <- plot(x, y.long, type="l", ylim=c(0,max(max(y.short),max(y.long))), main=paste("Long mean abx:", long_dur, 
                                                                      "days", "(", round(mean(rowSums(long_totalR)/iterations/n.bed), digits=4), ")"),
                         xlab="Time", ylab="% patients carrying MDRO", col=rgb(0,0,0,alpha=0.1))
     for(i in 2:iterations){
