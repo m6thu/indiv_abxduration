1464d9fb85e85840b9be183351eaeaee7beecbb9
Mo Yin <yin_mo@nuhs.edu.sg>
2019-08-01 12:22:44 +0100

    updated parameters heatmap to be based on ranking instead of p values

--- para_heatmap.R
+++ para_heatmap.R
@@ -26,12 +26,29 @@ models=c('Scenario A\n Model 1', 'Scenario A\n Model 2', 'Scenario A\n Model 3',
 
 #get data
 
-getp<-function(d, para.list, N, labs=labs.df){
-    prcc.wo.pvalues=d$prcc[[1]]$PRCC
-    dat = cbind.data.frame(d[['data']],unlist(d$res)[1:N])
-    prcc= cbind.data.frame(para.list, pvalues= as.numeric(epi.prcc(dat = dat, sided.test = 2)$p.value))
-    colnames(prcc)=c('parameters','p')
-    out=merge(labs.df,prcc, by='parameters', all.x=TRUE)[,c(1,3)]
+# getp<-function(d, para.list, N, labs=labs.df){
+#     prcc.wo.pvalues=d$prcc[[1]]$PRCC
+#     dat = cbind.data.frame(d[['data']],unlist(d$res)[1:N])
+#     prcc= cbind.data.frame(para.list, pvalues= as.numeric(epi.prcc(dat = dat, sided.test = 2)$p.value))
+#     colnames(prcc)=c('parameters','p')
+#     out=merge(labs.df,prcc, by='parameters', all.x=TRUE)[,c(1,3)]
+#     return(out)
+# }
+
+getposition<-function(data,labs=labs.df){
+    prcc= data$prcc[[1]]$PRCC
+    prcc$ranking=NA
+    prcc= prcc[order(prcc$original),] #arrange in ranking order
+    low = which(prcc$`min. c.i.` < 0 & prcc$`max. c.i.`< 0)
+    high= which(prcc$`min. c.i.` > 0 & prcc$`max. c.i.`> 0)
+    none= setdiff(1:nrow(prcc),c(low,high))
+    prcc$ranking[low]=prcc$`min. c.i.`[low]
+    prcc$ranking[high]=prcc$`max. c.i.`[high]
+    prcc$ranking[none]=0
+    
+    df=data.frame(parameters=rownames(prcc), ranking=prcc$ranking)
+    out=merge(labs.df, df, by='parameters', all.x=TRUE)[,c(1,3)]
+    
     return(out)
 }
 
@@ -42,27 +59,38 @@ Bmodel1=get(load('runs/LHS_simple_50013Jul2019_0057GMT.Rdata'))
 Bmodel2=get(load('runs/LHS_binary_50017Jul2019_0042GMT.Rdata')) 
 Bmodel3=get(load('runs/LHS_frequency_50017Jul2019_1721GMT.Rdata'))
 
-Amodel1.p=getp(Amodel1, para.list=parameters_simple, 500)
-Amodel2.p=getp(Amodel2, para.list=parameters_binary, 500)
-Amodel3.p=getp(Amodel3, para.list=parameters_freq, 800)
-Bmodel1.p=getp(Bmodel1, para.list=parameters_simple, 500)
-Bmodel2.p=getp(Bmodel2, para.list=parameters_binary, 500)
-Bmodel3.p=getp(Bmodel3, para.list=parameters_freq, 500)
+#Amodel1.p=getp(Amodel1, para.list=parameters_simple, 500)
+# Amodel2.p=getp(Amodel2, para.list=parameters_binary, 500)
+# Amodel3.p=getp(Amodel3, para.list=parameters_freq, 800)
+# Bmodel1.p=getp(Bmodel1, para.list=parameters_simple, 500)
+# Bmodel2.p=getp(Bmodel2, para.list=parameters_binary, 500)
+# Bmodel3.p=getp(Bmodel3, para.list=parameters_freq, 500)
+
+Amodel1.p= getposition(Amodel1)
+Amodel2.p= getposition(Amodel2)
+Amodel3.p= getposition(Amodel3)
+Bmodel1.p= getposition(Bmodel1)
+Bmodel2.p= getposition(Bmodel2)
+Bmodel3.p= getposition(Bmodel3)
 
 ##prepare data
 forplot=rbind.data.frame(Amodel1.p, Amodel2.p, Amodel3.p, 
                          Bmodel1.p, Bmodel2.p, Bmodel3.p)
 forplot$model= rep(models, each=length(parameters))
-forplot$p[grep('abx.r',forplot$parameters)[4:6]]=NA
-forplot$`p value`=rescale(forplot$p, to=c(0,1))
+forplot$ranking[grep('abx.r',forplot$parameters)[4:6]]=NA
+forplot$`ranking value`=rescale(forplot$ranking, to=c(0,1))
 
 base_size=9
 ggplot(forplot, aes(model, parameters)) + 
-    geom_tile(aes(fill = `p value`), colour = "white") + 
-    scale_fill_gradientn(colours=c("#7A306C","#068D9D","#9BC1BC"),
-                         values=c(0,0.001,0.05, 1), na.value = "white")+
+    geom_tile(aes(fill = ranking), colour = "white") + 
+    scale_fill_gradientn(colours=c("#388697",'#fbfae5',"#EB5160"),
+                         na.value = "white", 
+                         breaks=c(min(forplot$ranking, na.rm = T),0,max(forplot$ranking, na.rm = T)),
+                         labels=c('Positively affects\n outcome',
+                                  'Does not affect\n outcome', 
+                                  'Negatively affects\n outcome'))+
     theme_grey(base_size = base_size) + 
-    labs(x = "", y="")+ 
+    labs(x = "", y="", fill = "Ranking positions\n")+ 
     scale_x_discrete(expand = c(0, 0)) +
     scale_y_discrete(expand = c(0, 0)) + 
     theme(legend.position = "right", 
